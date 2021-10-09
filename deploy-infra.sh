1b4cf25c50b750a58c74774170c07d750f720869
Sibi John <sibijohn72@gmail.com>
2021-10-09 21:22:54 +0200

    added code build and deployment

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Generate a personal access token with repo and admin:repo_hook
+#    permissions from https://github.com/settings/tokens
+GH_ACCESS_TOKEN=$(cat ~/.github/aws-bootstrap-access-token)
+GH_OWNER=$(cat ~/.github/aws-bootstrap-owner)
+GH_REPO=$(cat ~/.github/aws-bootstrap-repo)
+GH_BRANCH=master
+
 STACK_NAME=awsbootstrap 
 REGION=ap-south-1 
 CLI_PROFILE=awsbootstrap
@@ -30,7 +37,12 @@ aws cloudformation deploy \
   --template-file main.yml \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
-  --parameter-overrides EC2InstanceType=$EC2_INSTANCE_TYPE
+  --parameter-overrides EC2InstanceType=$EC2_INSTANCE_TYPE \
+    GitHubOwner=$GH_OWNER \
+    GitHubRepo=$GH_REPO \
+    GitHubBranch=$GH_BRANCH \
+    GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
 
     # If the deploy succeeded, show the DNS name of the created instance
 if [ $? -eq 0 ]; then
